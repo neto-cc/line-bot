82a0eb640b8bf190ac26582b3e3549cdcc5ccab8
neto-cc <33005@iwaki-cc.jp>
2024-12-04 16:17:55 +0900

    Initial commit

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const config = {
 admin.initializeApp({
   credential: admin.credential.cert({
     
-    projectI
-projectId: process.env.FIREBASE_PROJECT_ID,
+	projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
