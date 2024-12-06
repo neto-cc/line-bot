c13311373caa267952f9b91076e212bab9937b5d
neto-cc <33005@iwaki-cc.jp>
2024-12-06 11:23:27 +0900

    Initial commit

--- index.js
+++ index.js
@@ -70,20 +70,13 @@ async function handleEvent(event) {
     if (userMessage.includes('年間行事')) {
       const replyMessage = 'こちらが年間行事の予定です:\nhttps://www.iwaki-cc.ac.jp/app/wp-content/uploads/2024/04/2024%E5%B9%B4%E9%96%93%E8%A1%8C%E4%BA%8B%E4%BA%88%E5%AE%9A-_%E5%AD%A6%E7%94%9F%E7%94%A8.pdf';
 
-      
-
-      con
-const feedbackTemplate = {
+      const feedbackTemplate = {
         type: 'template',
         altText: 'フィードバックのお願い',
         template: {
-          
-          t
-		type: 'buttons',
+          type: 'buttons',
           text: 'この情報は役に立ちましたか？',
-          
-          action
-actions: [
+          actions: [
             {
               type: 'postback',
               label: '役に立った',
@@ -98,20 +91,13 @@ actions: [
         },
       };
 
-      
-            },
-          ],
-        },
-  
-return client.replyMessage(event.replyToken, [
+      return client.replyMessage(event.replyToken, [
         { type: 'text', text: replyMessage },
         feedbackTemplate,
       ]);
     }
 
-    
-      
-// オープンキャンパスに該当するかチェック
+    // オープンキャンパスに該当するかチェック
     if (userMessage.includes('オープンキャンパス')) {
       const replyMessage = 'こちらがオープンキャンパスの予定です:\nhttps://www.iwaki-cc.ac.jp/app/wp-content/uploads/2024/04/%E3%83%9D%E3%82%B9%E3%82%BF%E3%83%BC%E6%9C%80%E7%B5%82PNG%E5%8C%96.png';
 
@@ -150,6 +136,7 @@ return client.replyMessage(event.replyToken, [
   // 他のイベントは無視
   return Promise.resolve(null);
 }
+
 // サーバー起動
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
